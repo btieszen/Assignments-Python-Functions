0b85652f29ab1fa0724bb4cd37b6c7fcddc300bc
btieszen <briantieszen4@gmail.com>
2024-07-03 15:01:56 -0700

    commit

--- task1.1.py
+++ task1.1.py
@@ -1,26 +1,40 @@
 #1. The Calculator App
 #Objective: The aim of this assignment is to build a basic calculator that can perform addition, subtraction, multiplication, and division.
-print("Hello, Welcome to your Calculator")
-while True:
+#Task 1: Create functions for each arithmetic operation.
+#Use inputs to ask the user what operation they want to perform, and what numbers they want to use.
+#Ensure your code can handle division by zero and other potential errors. So if you divide by 0, there is error handling set up to prevent an error from showing in the console.
 
-    operation = input("First enter the calculation you would like to perform: add,subtract,multiply, or divide: ")
-    first_num = int(input("Enter the first number: "))
-    second_num = int(input("enter second number: "))
-    add_num = (first_num + second_num)
-    subtract_num = (first_num - second_num) 
-    multiply_num = (first_num * second_num)
-    try:
-        divide_num = (first_num / second_num)
-    except ZeroDivisionError:
-        print("Can't divide by 0")
-    if (operation) == "add":
-        print (f"{first_num} + {second_num} = {add_num}")
-    elif (operation) == "subtract":
-        print (f"{first_num} - {second_num} = {subtract_num}")
-    elif (operation) == "multiply":
-        print (f"{first_num} * {second_num} = {multiply_num}")
-    elif (operation) == "divide":
-        print (f"Please enter a valid operation")
+operation =["+","-","*","/"]
+def basic_operation():
+    
+
+    while True:
+        try:
+            print("Welcome to your Calculator")
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number:  "))
+            task = input("Choose an operation(+,-,*,/)")
+            if task not in operation:
+    
+                print("Invalid opoeration please choose:(+,-,*,/)")
+
+            if task =="+":
+                result = num1+num2
+            elif task =="-":
+                result =num1-num2
+            elif task =="*":
+                result = num1*num2
+            elif task =="/":
+                result = num1/num2
+            print(F"The result is {result}") 
+        except ZeroDivisionError:
+            print("Can not divide by zero")
+basic_operation()
+           
+      
+        
+        
+
           
         
     
